373b875d136b5a634284f81c20368cbd9fadbcee
Hamza Bouyarman <hamzaclashroyale5@gmail.com>
2025-11-25 02:50:02 +0000

    first commit

---
+++ billing-service/src/main/java/org/example/billingservice/web/BillRestController.java
@@ -0,0 +1,36 @@
+package org.example.billingservice.web;
+
+import org.example.billingservice.entities.Bill;
+import org.example.billingservice.feign.CustomerRestClient;
+import org.example.billingservice.feign.ProductRestClient;
+import org.example.billingservice.repositories.BillRepository;
+import org.example.billingservice.repositories.ProductItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BillRestController {
+    @Autowired
+    private BillRepository billRepository;
+
+    @Autowired
+    private ProductItemRepository productItemRepository;
+
+    @Autowired
+    private CustomerRestClient customerRestClient;
+
+    @Autowired
+    private ProductRestClient productRestClient;
+
+    @GetMapping("/bills/{id}")
+    public Bill getBill(@PathVariable Long id){
+       Bill bill = billRepository.findById(id).get();
+       bill.setCustomer(customerRestClient.getCustomerById(bill.getId()));
+       bill.getProductsItems().forEach(productItem -> {
+           productItem.setProduct(productRestClient.getProductById(productItem.getProductId()));
+       });
+       return bill;
+    }
+}
